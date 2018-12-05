4f58def68e9493000ff6e27149956deeccf172e0
Nick <nickvandettethree@gmail.com>
2018-12-05 09:10:30 -0500

    main work done

--- trivia.py
+++ trivia.py
@@ -1,5 +1,5 @@
 import discord
-import threading
+import asyncio
 import urllib.request
 import html
 import random
@@ -8,7 +8,9 @@ import json
 import re
 
 responses = {
-    
+    '!answer': {
+        'usage': '**{}**, please enter the corresponding letter for your answer to the question.'
+    }
 }
 
 class InvalidCommandException(Exception):
@@ -27,7 +29,7 @@ class Command:
         tokens = message.content.split(' ')
 
         # Baseline acceptance criteria
-        if len(types) > len(tokens)-1:
+        if len(types) > len(tokens) - 1:
             # Command must have at least len(criteria) tokens.
             raise InvalidCommandException(responses[tokens[0]]['usage'].format(self.sender.name), location)
         
@@ -64,40 +66,78 @@ with open('../TOKEN') as tokenFile:
     TOKEN = tokenFile.readline()
 
 client = discord.Client()
+letters = ['a', 'b', 'c', 'd', 'e']
 currentQuestion = False
-question = ''
-botsChannel = ''
-gambleChannel = ''
+choices = None
+botsChannel = None
+gambleChannel = None
 
+@asyncio.coroutine
 async def sendQuestion():
-    global currentQuestion
-    global botsChannel
-    global gambleChannel
-    if not currentQuestion:
-        currentQuestion = True
+    global choices
+    if choices == None:
         question = json.loads(urllib.request.urlopen('https://opentdb.com/api.php?amount=1').read().decode('utf-8'))
-        choices = question['results'][0]['incorrect_answers']
-        choices.append(question['results'][0]['correct_answer'])
+        choices = [(choice, 0) for choice in question['results'][0]['incorrect_answers']]
+        choices.append((question['results'][0]['correct_answer'], 1))
         random.shuffle(choices)
-        letters = ['a', 'b', 'c', 'd']
-        body = '\n'.join(['({}) {}'.format(letters[i], html.unescape(choices[i])) for i in range(len(choices))])
+        body = '\n'.join(['({}) {}'.format(letters[i], html.unescape(choices[i][0])) for i in range(len(choices))])
         await send(gambleChannel, '*Trivia time!*')
         await sendRich(gambleChannel, html.unescape(question['results'][0]['question']), body, 0x666666)
-    timeDelay = random.randrange(60*15, 60*60*2)
+    timeDelay = random.randrange(60*60, 60*60*2)
     print('Waiting {} seconds for next trivia question.'.format(timeDelay))
-    threading.Timer(timeDelay, sendQuestion).start()
+    await asyncio.sleep(timeDelay)
+    await sendQuestion()
 
 @client.event
 async def on_message(message):
+    global choices
     # Ignore messages sent by a bot.
     if message.author.bot:
         return
     
     # DEBUG
-    print(str(message.content))
+    #print(str(message.content))
 
     if message.content.startswith('!answer'):
-        pass
+        try:
+            if choices == None:
+                await send(message.channel,
+                    'Sorry {}, there\'s no currently unanswered question.'
+                    .format(message.author.name))
+            else:
+                command = Command(message, [str])
+                sender = command.sender
+                letter = command.args[0]
+                if letter not in [letters[i] for i in range(len(choices))]:
+                    raise InvalidCommandException(responses['!answer']['usage'].format(sender.name), message.channel)
+                index = letters.index(letter)
+                if choices[index][1]:
+                    # Correct!
+                    await send(gambleChannel, random.choice([string.format(message.author.name) for string in [
+                        '**{}**, you got it!',
+                        '**{}** got it!',
+                        'Correct, **{}**!',
+                        '**{}** got it!',
+                        'Correct, **{}**!',
+                        'Right! Well done, **{}**.',
+                        'Ding! Good job, **{}**.'
+                    ]]))
+                    await send(botsChannel, '!reward 200 {}'.format(message.author.mention))
+                    await send(gambleChannel, 'You\'ve been rewarded 200 beans.')
+                    choices = None
+                else: # Incorrect!
+                    await send(gambleChannel, random.choice([
+                        'So close!',
+                        'Incorrect!',
+                        'Wrong!',
+                        'Nope!',
+                        'Wrong!',
+                        'Nope!',
+                        'Try again!'
+                    ]))
+        except InvalidCommandException as e:
+            await send(message.channel, e.value)
+
 
 @client.event
 async def on_ready():
@@ -106,7 +146,7 @@ async def on_ready():
     print('Logged in!')
     botsChannel = list(client.servers)[0].get_channel('514095418481704972')
     # TODO: Eventually change this to the right channel ID
-    gambleChannel = list(client.servers)[0].get_channel('514095418481704972')
+    gambleChannel = list(client.servers)[0].get_channel('514118083854467092')
     await sendQuestion()
         
 client.run(TOKEN)
